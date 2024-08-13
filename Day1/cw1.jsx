739beaf3db7a21f542412680d5426082c8f8a968
Puja-0307 <152993005+Puja-0307@users.noreply.github.com>
2024-08-13 09:46:43 +0530

    Update cw1.jsx

--- Day1/cw1.jsx
+++ Day1/cw1.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-
-function ShowAlert() {
-  alert ("Message From Javascript Alert");
-  console.log("Message from Developer");
-};
-
-export default ShowAlert;
+function AlertMsg()
+{
+    alert("Message from Javascript Alert");
+    console.log("Message to developer");
+}
+function App()
+{
+    return(
+        <div className="App">
+            <h1>Let us see the output of the Javascript</h1>
+            <button onClick={AlertMsg}>Click</button><br></br><br></br>
+        </div>
+    )
+}
+export default App;
